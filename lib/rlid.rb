fc79a3df0460661966a3f5d4d4d70b565214cd0d
Fela Winkelmolen <fela.kde@gmail.com>
2010-04-26 11:47:11 +0200

    typo

--- lib/rlid.rb
+++ lib/rlid.rb
@@ -1,6 +1,6 @@
 require 'rlid/language_guesser/naive_bayes_guesser'
 
-class Rlid
+module Rlid
   @guesser = NaiveBayesProbabilityGuesser.new
   def self.guess_language(string)
     @guesser.guess_language(string)
